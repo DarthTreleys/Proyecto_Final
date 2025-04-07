8b4a6a96032052329f963d92828dfa5324600084
n4g0z3 <ivangonzalezg05@gmail.com>
2025-04-07 15:27:50 +0200

    Update Eliminacio_de_fitxers_temporals.py

--- scripts/Eliminacio_de_fitxers_temporals.py
+++ scripts/Eliminacio_de_fitxers_temporals.py
@@ -1,12 +1,6 @@
 import os
 import platform
 import sys
-import random
-import time
-import requests
-import csv
-from bs4 import BeautifulSoup
-from datetime import datetime
 ### Eliminació de fitxers temporals
 directorio = input("Indica el directori") #formulario que especifique directorio (php)
     
