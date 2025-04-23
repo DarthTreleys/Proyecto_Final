042504fe407d0d50aba4737de994f07a03d778a3
n4g0z3 <ivangonzalezg05@gmail.com>
2025-04-23 16:36:54 +0200

    Update db.php

---
+++ Projecte/db.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$host="localhost";
+$usuari="root";
+$contrasenya="";
+$bbdd="ProjecteFinal";
+  
+$connexio=new mysqli($host, $usuari, $contrasenya, $bbdd)
+  
+if ($conn->connect_error) {
+    die("Error de connexio " . $conn->connect_error);
+}
+?>
