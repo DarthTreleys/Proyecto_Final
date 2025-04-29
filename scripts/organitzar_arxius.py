fccc8aae6f60ade186435e3f587eedc95ccaa5c9
n4g0z3 <ivangonzalezg05@gmail.com>
2025-04-29 15:30:58 +0200

    Update organitzar_arxius.py

--- scripts/organitzar_arxius.py
+++ scripts/organitzar_arxius.py
@@ -4,13 +4,28 @@ import sys
 ##Programa que organitzi els arxius en un directori, movent-los a subdirectoris en base la seva extensió
 directori = input("Directori: ")
 
+so = platform.system()
+
+if so="Windows":
 for arxiu in os.listdir(directori):
     ruta = os.path.join(directori, arxiu)
     if os.path.isfile(ruta):
         extension = arxiu.split('.')[-1] 
         if extension == "png":
-            os.system("move " +ruta+ r" C:\Users\DEEPGAMING\Pictures")
+            os.system("move " +ruta+ " ~\\Pictures")
         if extension == "mp4":
-            os.system("move " +ruta+ " C:\\Users\\DEEPGAMING\\Videos")
+            os.system("move " +ruta+ "~\\Videos")
         if extension == "mp3":
-            os.system("move " +ruta+ " C:/Users/DEEPGAMING/Music")
+            os.system("move " +ruta+ "~\\Music")
+
+elif so="Linux":
+for arxiu in os.listdir(directori):
+    ruta = os.path.join(directori, arxiu)
+    if os.path.isfile(ruta):
+        extension = arxiu.split('.')[-1].lower()
+        if extension == "png":
+            os.system("mv " +ruta+ " ~/Pictures")
+        if extension == "mp4":
+            os.system("mv " +ruta+ "~/Videos")
+        if extension == "mp3":
+            os.system("mv " +ruta+ "~/Music")
