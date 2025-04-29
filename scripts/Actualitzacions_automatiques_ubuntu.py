a2a5cfbf7ba5a9bba8f862630cf54185734df034
n4g0z3 <ivangonzalezg05@gmail.com>
2025-04-29 15:13:42 +0200

    Update Actualitzacions_automatiques.py

--- scripts/Actualitzacions_automatiques_ubuntu.py
+++ scripts/Actualitzacions_automatiques_ubuntu.py
@@ -1,5 +1,8 @@
 import subprocess
+import so
 
+so = platform.system()
+if so="Linux":
 print("Actualizando lista de paquetes...")
 subprocess.run(["sudo", "apt", "update"], check=True)
 
@@ -10,3 +13,17 @@ print("Limpieza de paquetes innecesarios...")
 subprocess.run(["sudo", "apt", "autoremove", "-y"], check=True)
 
 print("Actualización completada con éxito.")
+
+elif so="Windows":
+print("Buscando actualizaciones...")
+resultado = subprocess.run(["powershell", "-Command", "Get-WindowsUpdate"], capture_output=True, text=True)
+print("Actualizaciones disponibles:\n", resultado.stdout)
+
+print("Instalando actualizaciones...")
+resultado = subprocess.run(["powershell", "-Command", "Install-WindowsUpdate -AcceptAll -AutoReboot"], capture_output=True, text=True)
+print("Instalando actualizaciones:\n", resultado.stdout)
+
+print("Proceso completado.")
+
+else: 
+print("Lo sentimos, su sistema no es compatible con este script.")
