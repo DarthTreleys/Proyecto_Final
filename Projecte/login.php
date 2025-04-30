c0a0b428fd021c7e0bb67bbf55cb61a94c1b1b1f
Treleys <joancosdo@gmail.com>
2025-04-30 16:12:55 +0200

    Update login.php

--- Projecte/login.php
+++ Projecte/login.php
@@ -36,6 +36,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <p> No tens compte? <a href="register.php">Registra't aqui</a></p>
 </body>
 </html>*/
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 require 'db.php';
 
