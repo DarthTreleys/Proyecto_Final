6c66a4eda8cd0132961088701b844b54acd924e7
Treleys <joancosdo@gmail.com>
2025-05-05 15:36:29 +0200

    Update login.php

--- Projecte/login.php
+++ Projecte/login.php
@@ -1,41 +1,4 @@
 <?php
-/*session_start();
-require 'db.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $usuari = $_POST['usuari'] ?? '';
-  $passwd = $_POST['passwd'] ?? '';
-
-  $stmt = $pdo->prepare("SELECT * FROM usuaris WHERE User = ? AND Password = ?");
-  $stmt->execute([$usuari, $passwd]);
-  $user = $stmt->fetch();
-
-  if ($user) {
-    $_SESSION['usuari'] = $user['usuari'];
-    header('Location: index.php');
-    exit;
-  } else {
-    $error = "Usuari o contrasenya incorrecte.";
-  }
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Iniciar sessio</title>
-</head>
-<body>
-  <h2>Login</h2>
-  <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
-  <form method="POST">
-    <input type="text" name="usuari" placeholder="Usuari" required><br><br>
-    <input type="password" name="passwd" placeholder="Contrasenya" required><br><br>
-    <button type="submit">Entrar</button>
-  </form>
-  <p> No tens compte? <a href="register.php">Registra't aqui</a></p>
-</body>
-</html>*/
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
