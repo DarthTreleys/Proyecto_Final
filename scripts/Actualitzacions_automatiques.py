c9ec75366bfc4992ba5970889e3ac7c0d5041269
Treleys <joancosdo@gmail.com>
2025-05-05 15:30:26 +0200

    Update Actualitzacions_automatiques.py

--- scripts/Actualitzacions_automatiques.py
+++ scripts/Actualitzacions_automatiques.py
@@ -14,16 +14,5 @@ subprocess.run(["sudo", "apt", "autoremove", "-y"], check=True)
 
 print("Actualización completada con éxito.")
 
-elif os.name="nt":
-print("Buscando actualizaciones...")
-resultado = subprocess.run(["powershell", "-Command", "Get-WindowsUpdate"], capture_output=True, text=True)
-print("Actualizaciones disponibles:\n", resultado.stdout)
-
-print("Instalando actualizaciones...")
-resultado = subprocess.run(["powershell", "-Command", "Install-WindowsUpdate -AcceptAll -AutoReboot"], capture_output=True, text=True)
-print("Instalando actualizaciones:\n", resultado.stdout)
-
-print("Proceso completado.")
-
 else: 
-print("Lo sentimos, su sistema no es compatible con nuestros servicios")
+print("Lo sentimos, su sistema no es compatible con nuestra aplicación.")
