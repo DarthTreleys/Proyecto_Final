de4a43c991179904325e7f51b0ad4b92ac1f865a
n4g0z3 <ivangonzalezg05@gmail.com>
2025-05-06 15:43:45 +0200

    Update Wifis_sin_passwd.py

--- scripts/Wifis_sin_passwd.py
+++ scripts/Wifis_sin_passwd.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 
-if os.name == 'posix':
     result = subprocess.run(['nmcli', '-f', 'SSID,SECURITY', 'dev', 'wifi'], stdout=subprocess.PIPE)
     output = result.stdout.decode()
 
@@ -14,16 +13,3 @@ if os.name == 'posix':
                 subprocess.run(['nmcli', 'connection', 'delete', ssid])
                 print(f"Red abierta eliminada o bloqueada: {ssid}")
 
-elif os.name == 'nt':
-    result = subprocess.run(['netsh', 'wlan', 'show', 'networks', 'mode=bssid'], stdout=subprocess.PIPE)
-    output = result.stdout.decode(errors='ignore')
-
-    for line in output.split('\n'):
-        if 'SSID' in line:
-            ssid = line.split(":", 1)[1].strip()
-        if 'Authentication' in line:
-            auth = line.split(":", 1)[1].strip()
-            if auth.lower() == 'open':
-                print(f"Red abierta detectada: {ssid}")
-                subprocess.run(['netsh', 'wlan', 'delete', 'profile', f'name="{ssid}"'])
-                print(f"Perfil eliminado (si existía): {ssid}")
