fd4bd156c0cf199735c4baeb4f22bf8db0e92cf1
n4g0z3 <ivangonzalezg05@gmail.com>
2025-05-07 16:07:35 +0200

    Update Crear_copies_de_seguretat.py

--- scripts/Crear_copies_de_seguretat.py
+++ scripts/Crear_copies_de_seguretat.py
@@ -1,13 +1,15 @@
 import os
-import platform
-import sys
-### Crear copies de seguretat
+import shutil
+
 SOURCE = "/home"
-DEST = "/"
+DEST = "/backup" 
 
 for root, _, files in os.walk(SOURCE):
     for file in files:
         src = os.path.join(root, file)
         dst = os.path.join(DEST, os.path.relpath(src, SOURCE))
+
+        os.makedirs(os.path.dirname(dst), exist_ok=True) 
+        shutil.copy2(src, dst)  
 
 print("Còpia completada.")
