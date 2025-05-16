6d9ad26ff939d9b87c3fffb011b02a68cdceaf65
n4g0z3 <ivangonzalezg05@gmail.com>
2025-05-16 13:08:43 -0400

    Update backup.py

--- scripts/backup.py
+++ scripts/backup.py
@@ -4,13 +4,12 @@ import subprocess
 SOURCE = "/home"
 DEST = "/backup"
 
+subprocess.run(["sudo", "mkdir", "-p", "backup"])
+
 for root, _, files in os.walk(SOURCE):
-    for file in files:
-        src = os.path.join(root, file)
-        dst = os.path.join(DEST, os.path.relpath(src, SOURCE))
-        dst_dir = os.path.dirname(dst)
+        for file in files:
+                src = os.path.join(root, file)
+                dst = os.path.join(DEST, os.path.relpath(src, SOURCE))
+                subprocess.run(["sudo", "cp", "-p", src, dst], check=True)
+        print("Còpia completada.")
 
-        subprocess.run(["sudo", "mkdir", "-p", dst_dir], check=True)
-        subprocess.run(["sudo", "cp", "-p", src, dst], check=True)
-
-print("Còpia completada.")
