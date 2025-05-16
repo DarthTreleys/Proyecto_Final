70c2cce144716149a6697337df5c5cd1046a07d1
n4g0z3 <ivangonzalezg05@gmail.com>
2025-05-16 13:10:34 -0400

    Update organitzar_arxius.py

--- scripts/organitzar_arxius.py
+++ scripts/organitzar_arxius.py
@@ -1,17 +1,22 @@
 import os
 import platform
 import sys
-##Programa que organitzi els arxius en un directori, movent-los a subdirectoris en base la seva extensió
+
 directori = "/home"
 
-if os.name="nt":
 for arxiu in os.listdir(directori):
     ruta = os.path.join(directori, arxiu)
     if os.path.isfile(ruta):
         extension = arxiu.split('.')[-1] 
         if extension == "png":
-            os.system("move " +ruta+ " ~\\Pictures")
+            os.system("mv" +ruta+ " ~/Pictures")
+            print("Fitxer "+ruta+" Mogut a Pictures")
         if extension == "mp4":
-            os.system("move " +ruta+ "~\\Videos")
+            os.system("mv " +ruta+ " ~/Videos")
+            print("Fitxer " +ruta+ " Mogut a Videos")
         if extension == "mp3":
-            os.system("move " +ruta+ "~\\Music")
+            os.system("mv " +ruta+ " ~/Music")
+            print("Fitxer "+ruta+" Mogut a Music")
+    else:
+       print("No tens pngs, mp3s o mp4s en el directori")
+
